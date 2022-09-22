5bff1292422758bb2a8c57bf3d3a82e438fa698b
BossPaida <paidachikumbu@gmail.com>
2022-09-22 07:08:16 +0200

    controller error

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\models\ListItem;
 
 class TodoListController extends Controller
 {
-    //
+   public function saveItem(Request $request){
+    $newListItem = new ListItem;
+    $newListItem->name = $request->listItem;
+    $newListItem->is_complete = 0; 
+    $newListItem->save();
+
+    return view('welcome');
+   }
 }
